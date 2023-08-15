6dafe23d6eed35ebc71d641213ddfaa04179f83b
jonas <jonas@JONASNOTEBOOK>
2023-08-15 18:13:54 +0200

    Projektdateien hinzufügen.

---
+++ imagecropper/Controllers/imageCropperController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ImageCropperApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ImageCropperController : ControllerBase
+    {
+        private readonly ImageCropper _imageCropper;
+
+        public ImageCropperController(ImageCropper imageCropper)
+        {
+            _imageCropper = imageCropper;
+        }
+
+        [HttpPost("crop")]
+        public async Task<IActionResult> CropImage([FromBody] ImageProcessRequest processRequest)
+        {
+            try
+            {
+                byte[] processedImage = await _imageCropper.CropImage(processRequest.ImageUrl, processRequest.Width, processRequest.Height, processRequest.StartX, processRequest.StartY);
+
+                return File(processedImage, "image/jpeg"); // Ändern Sie den MIME-Typ je nach Bildformat
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+
+        [HttpPost("resize")]
+        public async Task<IActionResult> ResizeImage([FromBody] ImageProcessRequest processRequest)
+        {
+            try
+            {
+                byte[] processedImage = await _imageCropper.ResizeImage(processRequest.ImageUrl, processRequest.Width, processRequest.Height);
+
+                return File(processedImage, "image/jpeg"); // Ändern Sie den MIME-Typ je nach Bildformat
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+
+        [HttpPost("rotate")]
+        public async Task<IActionResult> RotateImage([FromBody] ImageProcessRequest processRequest)
+        {
+            try
+            {
+                byte[] processedImage = await _imageCropper.RotateImage(processRequest.ImageUrl, processRequest.RotateDegree);
+
+                return File(processedImage, "image/jpeg"); // Ändern Sie den MIME-Typ je nach Bildformat
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+
+
+    }
+
+    public class ImageProcessRequest
+    {
+        public string? ImageUrl { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int StartX { get; set; }
+        public int StartY { get; set; }
+        public int RotateDegree { get; set; }
+    }
+}
---
+++ imagecropper/ImageCropper.cs
@@ -0,0 +1,78 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using System.IO;
+using System.Threading.Tasks;
+
+    public class ImageCropper
+    {
+
+        public async Task<byte[]> CropImage(string imageUrl, int width, int height, int startX, int startY)
+        {
+            byte[] imageBytes = await APIRequestAsync(imageUrl);
+
+            using (MemoryStream inputStream = new MemoryStream(imageBytes))
+            using (Image image = Image.Load(inputStream))
+            {
+                image.Mutate(x => x.Crop(new Rectangle(startX, startY, width, height)));
+                using (MemoryStream outputStream = new MemoryStream())
+                {
+                    image.Save(outputStream, new JpegEncoder());
+                    return outputStream.ToArray();
+                }
+            }
+        }
+        public async Task<byte[]> ResizeImage(string imageUrl, int width, int height)
+        {
+            byte[] imageBytes = await APIRequestAsync(imageUrl);
+
+            using (MemoryStream inputStream = new MemoryStream(imageBytes))
+            using (Image image = Image.Load(inputStream))
+            {
+                image.Mutate(x => x.Resize(new Size(width, height)));
+                using (MemoryStream outputStream = new MemoryStream())
+                {
+                    image.Save(outputStream, new JpegEncoder());
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
+        public async Task<byte[]> RotateImage(string imageUrl, int rotateDegree)
+        {
+            byte[] imageBytes = await APIRequestAsync(imageUrl);
+
+            using (MemoryStream inputStream = new MemoryStream(imageBytes))
+            using (Image image = Image.Load(inputStream))
+            {
+                image.Mutate(x => x.Rotate(rotateDegree));
+                using (MemoryStream outputStream = new MemoryStream())
+                {
+                    image.Save(outputStream, new JpegEncoder());
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
+    private async Task<byte[]> APIRequestAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using (Stream imageStream = await response.Content.ReadAsStreamAsync())
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        await imageStream.CopyToAsync(memoryStream);
+                        return memoryStream.ToArray();
+                    }
+                }
+                else
+                {
+                    throw new Exception($"Error fetching image from URL: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+        }
+    }
